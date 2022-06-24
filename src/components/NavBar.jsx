e64cdb69b7d0a1629dcc28b459785a05e845a1b5
Antonia Castro <mariantonita@gmail.com>
2022-06-24 11:33:53 -0400

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div class="ui tabular menu">
+        <a class="item">Cats Photos</a>
+        <a class="item active">Dog Photos</a>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
